2b0a7ffc926743c70764d548c322331a1fa59a2e
Rushil <workwithrushil@gmail.com>
2025-08-16 16:44:41 +0530

    cors-policy

--- server/index.js
+++ server/index.js
@@ -5,14 +5,23 @@ const cors=require('cors');
 const server=http.createServer(app);
 const {Server}=require('socket.io');
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",               
+    "https://react-chat-app-q6tt.vercel.app"
+  ],
+  methods: ["GET", "POST"]
+}));
 
 const io = new Server(server, {
-    cors: {
-        origin: ["https://react-chat-app-q6tt.vercel.app/", "http://localhost:5173"],
-        methods: ["GET", "POST"]
-    },
-})
+  cors: {
+    origin: [
+      "http://localhost:5173", 
+      "https://react-chat-app-q6tt.vercel.app"
+    ],
+    methods: ["GET", "POST"]
+  }
+});
 
 server.listen(5000, ()=>{
     console.log("Server listening on port 5000");
